fc0974a3d72bc9408763ada9689df026d060ca84
Alessandro Rosà <alessandro.rosa@sdfgroup.com>
2026-03-07 07:55:56 +0100

    refactor: update personal information and links to reflect new identity

--- app/components/ProgrammingMetrics.vue
+++ app/components/ProgrammingMetrics.vue
@@ -13,8 +13,7 @@ defineProps<{
     <p class="text-xl text-black-primary dark:text-white-primary">
       This week I have been writing a lot of
       {{ metrics?.languages?.[0]?.name ?? 'Rust' }} using
-      {{ metrics?.editors?.[0]?.name ?? 'Neovim' }} on my machine running
-      {{ metrics?.operating_systems?.[0]?.name ?? 'Linux' }}.
+      {{ metrics?.editors?.[0]?.name ?? 'Neovim' }}
     </p>
 
     <div class="grid grid-cols-1 gap-4 xl:grid-cols-3">
@@ -24,13 +23,6 @@ defineProps<{
 
       <Tile v-if="metrics?.editors?.length" class="mx-auto w-full">
         <ChartsBar :minimum-percent="0.5" :points="metrics?.editors ?? []" />
-      </Tile>
-
-      <Tile v-if="metrics?.operating_systems?.length" class="mx-auto w-full">
-        <ChartsBar
-          :minimum-percent="0.5"
-          :points="metrics?.operating_systems ?? []"
-        />
       </Tile>
     </div>
   </section>
